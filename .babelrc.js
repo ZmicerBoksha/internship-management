349f37a4339372b560552e63d273d3225de882a0
alibek-begnaev <alibek.begnaev@gmail.com>
2021-03-30 10:25:10 +0500

    added eslint, changed structure

--- .babelrc.js
+++ .babelrc.js
@@ -1,3 +1,15 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-typescript', '@babel/preset-react']
+  presets: [
+    "@babel/preset-env",
+    "@babel/preset-react",
+    "@babel/preset-typescript",
+  ],
+  plugins: [
+    [
+      "@babel/plugin-transform-runtime",
+      {
+        regenerator: true,
+      },
+    ],
+  ],
 };
