9ded123a07a6f14826f12ee0702148f1a729e42c
alibek-begnaev <alibek.begnaev@gmail.com>
2021-04-06 14:23:52 +0500

    tranings

--- src/components/HomePage/Trainings/Trainings.tsx
+++ src/components/HomePage/Trainings/Trainings.tsx
@@ -27,18 +27,81 @@ const useStyles = makeStyles((theme: Theme) =>
     },
   })
 );
-
+interface DetailType {
+  title: string;
+  location: string;
+  startDate: string;
+  duration: number;
+  id: number;
+}
+interface IData {
+  data: DetailType[];
+}
+const myData: IData = {
+  data: [
+    {
+      title: "Front",
+      location: "Tashkent",
+      startDate: "12/12/12",
+      duration: 2,
+      id: 1,
+    },
+    {
+      title: "Back",
+      location: "Moskov",
+      startDate: "12/12/12",
+      duration: 2,
+      id: 2,
+    },
+    {
+      title: "Front",
+      location: "Minsk",
+      startDate: "12/12/12",
+      duration: 2,
+      id: 3,
+    },
+  ],
+};
 const Trainings = () => {
   const classes = useStyles();
+
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
-        <Grid item xs={12} sm={4}>
+        {myData.data.map((detail: DetailType) => {
+          return (
+            <Grid key={detail.id} item xs={12} sm={4}>
+              <Paper className={classes.paper}>
+                <Typography
+                  className={classes.title}
+                  variant="h5"
+                  component="h1"
+                >
+                  {detail.title}
+                </Typography>
+                <ul style={{ listStyleType: "none", padding: 0 }}>
+                  <li>
+                    <Typography
+                      className={classes.subTitle}
+                      variant="subtitle1"
+                      component="h1"
+                    >
+                      {detail.location}
+                    </Typography>
+                  </li>
+                </ul>
+                <Button>Register</Button>
+              </Paper>
+            </Grid>
+          );
+        })}
+
+        {/* <Grid item xs={12} sm={4}>
           <Paper className={classes.paper}>
             <Typography className={classes.title} variant="h5" component="h1">
-              Front-End Developer
+              asdfasdf
             </Typography>
-            <ul style={{ listStyleType: "none", padding: 0 }}>
+            <ul>
               <li>
                 <Typography
                   className={classes.subTitle}
@@ -49,7 +112,6 @@ const Trainings = () => {
                 </Typography>
               </li>
             </ul>
-            <Button>Register</Button>
           </Paper>
         </Grid>
         <Grid item xs={12} sm={4}>
@@ -69,25 +131,7 @@ const Trainings = () => {
               </li>
             </ul>
           </Paper>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <Paper className={classes.paper}>
-            <Typography className={classes.title} variant="h5" component="h1">
-              asdfasdf
-            </Typography>
-            <ul>
-              <li>
-                <Typography
-                  className={classes.subTitle}
-                  variant="subtitle1"
-                  component="h1"
-                >
-                  asdfasdf
-                </Typography>
-              </li>
-            </ul>
-          </Paper>
-        </Grid>
+        </Grid> */}
       </Grid>
     </div>
   );
--- src/components/TrainingDetailsPage/TrainingDetailsPage.tsx
+++ src/components/TrainingDetailsPage/TrainingDetailsPage.tsx
@@ -14,6 +14,7 @@ const useStyles = makeStyles((theme: Theme) =>
     },
   })
 );
+
 const TrainingDetailsPage = () => {
   const classes = useStyles();
   return (
